4d8fa6f368c9bc0b054ada9240385f8c02573f2f
Steven M Raines <stevenmraines@gmail.com>
2019-02-24 16:32:16 -0500

    Last commit.

--- app/Collection.php
+++ app/Collection.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Laravel\Scout\Searchable;
 
 class Collection extends Model
@@ -15,5 +16,26 @@ class Collection extends Model
 
     public function shows() {
         return $this->belongsToMany('App\Show', 'collection_shows');
+    }
+
+    public static function recent($limit) {
+        // $collectionMoviesIds =
+        //     DB::table('collection_movies')
+        //         ->orderBy('created_at', 'desc')
+        //         ->select('collection_id', 'created_at')
+        //         ->get();
+        //
+        // $uniqueCollectionMoviesIds = $collectionMoviesIds->unique(function($item) {
+        //     return $item[]
+        // });
+        //
+        // $collectionShowIds =
+        //     DB::table('collection_shows')
+        //         ->orderBy('created_at', 'desc')
+        //         ->select('collection_id', 'created_at')
+        //         ->get();
+        //
+        // $collectionIds = $collectionMoviesIds->merge($collectionShowIds);
+        // return $collectionIds;
     }
 }

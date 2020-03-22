ec21564c42a935bdb10a8d77db6a049f6c0cc277
Steven M Raines <stevenmraines@gmail.com>
2020-03-22 11:52:59 -0400

    Initial commit.

--- database/migrations/2020_03_22_153958_create_files_table.php
+++ database/migrations/2020_03_22_153958_create_files_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDriveEpisodeTable extends Migration
+class CreateFilesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,14 @@ class CreateDriveEpisodeTable extends Migration
      */
     public function up()
     {
-        Schema::create('drive_episode', function (Blueprint $table) {
-            $table->unsignedInteger('episode_id');
-            $table->unsignedInteger('drive_id');
+        Schema::table('files', function (Blueprint $table) {
+            $table->increments('id');
             $table->string('filename');
+            $table->unsignedInteger('width')->default(0);
+            $table->unsignedInteger('height')->default(0);
+            $table->unsignedInteger('duration')->default(0);
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
-            $table->foreign('drive_id')->references('id')->on('drives')->onDelete('cascade');
-            $table->foreign('episode_id')->references('id')->on('episodes')->onDelete('cascade');
-            $table->unique(['drive_id', 'episode_id']);
         });
     }
 
@@ -32,6 +31,8 @@ class CreateDriveEpisodeTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('drive_episode');
+        Schema::table('files', function (Blueprint $table) {
+            //
+        });
     }
 }
--- resources/assets/js/components/PosterContainer.vue
+++ resources/assets/js/components/PosterContainer.vue
@@ -19,22 +19,39 @@
 
 <script>
 	export default {
-		/*
-		 * Use local event dispatcher to prevent
-		 * all posters on all rows from being shifted
-		 */
 		props: [
-			'id',
-			'title',
-			'poster',
 			'eventDispatcher',
+			'id',
+			'poster',
+			'title',
+			'user',
 		],
+
+		computed: {
+			progress: function() {
+				if(user.history_movie) {
+					for(var i = 0; i < user.history_movie.length; i++) {
+						if(user.history_movie[i].id === this.id) {
+							return user.history_movie[i].pivot.progress;
+						}
+					}
+				}
+
+				if(user.episode_history) {
+
+				}
+			}
+		},
 
 		methods: {
 			getMedia() {
 				Event.trigger('getMedia', this.id);
 			},
 
+			/*
+			 * Use local event dispatcher for hover and unhover to
+			 * prevent all posters on all rows from being shifted.
+			 */
 			hover() {
 				if(typeof this.eventDispatcher.$emit === 'function') {
 					this.eventDispatcher.$emit('posterContainerHover', { id: this.id });
